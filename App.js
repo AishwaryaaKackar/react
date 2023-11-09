1e0619a4f692647e6b089a12810c7772a04a401a
Aishwaryaa <aishwaryaa22kackar@gmail.com>
2023-11-10 01:51:00 +0530

    Assignment 2 changes

--- App.js
+++ App.js
@@ -1,5 +1,32 @@
-const heading = React.createElement("h1",{
-    id:"heading"
-},"Hello World from React");
-        const root = ReactDOM.createRoot(document.getElementById("root"));
-        root.render(heading);
+import  React from "react";
+import  ReactDOM  from "react-dom/client";
+
+// const heading = React.createElement("div",{id:"parent"},
+// [
+//     React.createElement("div",{id:"child"},[
+//         React.createElement("h1",{},"Hello from React"),
+//         React.createElement("h2",{},"Hello h2 from React")
+//     ])
+// ]);    
+// console.log(heading);
+const jsxHead=(
+    <div>
+    <h1 id="heading">Hello from jsx heading</h1>
+    </div>
+);
+
+const Title = () => {
+    return <h1> This is a heading with a functional component</h1>
+}
+
+const HeadingComponent = function(){
+    return (<div>
+        <h1>This is heading component</h1>
+        <h2>This is sub heading</h2>
+        <Title></Title>
+        {jsxHead}
+    </div>)
+}
+console.log(jsxHead);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<HeadingComponent/>);
